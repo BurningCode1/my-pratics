7bfdcc5eacdf6ee2f580df3edebd37ea38dcc60f
BurningCode1 <154074703+BurningCode1@users.noreply.github.com>
2024-07-27 19:32:24 +0000

    add version in endpoint and corrige database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,16 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencias: {
+      database: {
+        version: databaseVersionValue,
+      },
+    },
+  });
 }
